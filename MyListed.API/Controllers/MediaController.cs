302653fc6aaca631301121a25422421c39d6c09c
Luiz Filipe <lf360.dev@gmail.com>
2026-02-09 14:09:14 -0300

    Adicionar arquivos de projeto.

---
+++ MyListed.API/Controllers/MediaController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyListed.API.Models;
+
+namespace MyListed.API.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class MediaController : ControllerBase
+{
+    private static List<Media> _lista = new List<Media>();
+    static int _nextId = 1;
+
+    [HttpGet]
+    public IEnumerable<Media> GetAllMedia()
+    {
+        return _lista;
+    }
+
+    [HttpPost]
+    public IActionResult PostMedia([FromBody] Media media)
+    {
+        media.Id = _nextId++;
+        _lista.Add(media);
+        return CreatedAtAction(nameof(GetAllMedia), new { id = media.Id }, media);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult PutMedia(int id, [FromBody] Media media)
+    {
+        var item = _lista.FirstOrDefault(m => m.Id == id);
+        if (item == null)
+        {
+            return NotFound();
+        }
+        item.Title = media.Title;
+        item.Description = media.Description;
+        item.Year = media.Year;
+        item.Type = media.Type;
+        return NoContent();
+    }
+
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteMedia(int id)
+    {
+        var item = _lista.FirstOrDefault(m => m.Id == id);
+        if (item == null)
+        {
+            return NotFound();
+        }
+        _lista.Remove(item);
+        return NoContent();
+    }
+}
---
+++ MyListed.API/Models/Media.cs
@@ -0,0 +1,10 @@
+﻿namespace MyListed.API.Models;
+
+public class Media
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public int Year { get; set; }
+    public string Type { get; set; }
+    public string Description { get; set; }
+}
